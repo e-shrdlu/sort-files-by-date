107c4e43285aa74e1073a18fa60173c04eb422d4
george <george@gyrotechie.com>
2019-05-29 22:57:38 -0500

    add command line args and print statements

--- image_sorter.py
+++ image_sorter.py
@@ -1,35 +1,40 @@
 import os
 import time
 import shutil
+import sys
 
+seperating_char='/'
 
-wordMonthToNum = {'January':'01', "February":'02', "March":'03', 'April':'04', 'May':'05', 'June':'06', 'July':'07',
-                  'August':'08', 'September':'09', 'October':'10', 'November':'11', 'December':'12'}
-
-
-def sort_files_by_month(seperating_char='\\', valid_file_types=('.jpg','.jpeg','.png','.avi','.mov')):
-    for subdir,dirs,files in os.walk('.'):
+def sort_files_by_month(copyFromDir, copyToDir, valid_file_types=('.jpg','.jpeg','.png','.avi','.mov','.thm')):
+    if len(sys.argv) > 1:
+        copyFromDir = sys.argv[1]
+    print('Searcing for photos in: ', copyFromDir, '  and copy any found to: ', copyToDir)
+    numCopied = 0
+    numDuplicates = 0
+    for subdir,dirs,files in os.walk(copyFromDir):
         for file in files:
-            if any(file.endswith(x) for x in valid_file_types):  # checks if file ends with any of the values in the list
+            if any(file.lower().endswith(x) for x in valid_file_types):  # checks if file ends with any of the values in the list
                 fn = subdir + seperating_char + file
-                dateCreated = str(time.ctime(os.path.getctime(fn))).split()
-                # 0day, 1month, 2numday, 3time(24:60:60), 4yr
-                newDir = wordMonthToNum[dateCreated[1]] + ' ' + dateCreated[1] + ' ' + dateCreated[4]
+                dateCreated = os.stat(fn).st_birthtime
+                newDir = time.strftime(copyToDir + '/%Y' + seperating_char + '%m %B', time.localtime(dateCreated))
                 newFn = newDir + seperating_char + file
                 os.makedirs(newDir, exist_ok=True)
                 dupeCount = 1
                 while True:
                     if not os.path.isfile(newFn):  # False if file exists already
                         shutil.copy2(fn, newFn)
+                        print('COPIED  {oldLocation}  to  {newLocation}'.format(oldLocation=fn, newLocation=newFn))
+                        numCopied += 1
                         break
                     elif os.path.getsize(fn) == os.path.getsize(newFn):  # size of files is equal so files are assumed to be the same
+                        print('DUPLICATE  {oldLocation}  to  {newLocation}'.format(oldLocation=fn, newLocation=newFn))
+                        numDuplicates += 1
                         break
                     else:  # file is dupe. add num to end
-                        newFn = newDir + seperating_char + (str(dupeCount) + '.').join(file.split('.'))
+                        newFn = newDir + seperating_char + ('-' + str(dupeCount) + '.').join(file.split('.'))
                         dupeCount += 1
                         continue
-                print('moved {oldLocation} to {newLocation}'.format(oldLocation=fn, newLocation=newFn))
-
+        print('Copied: ', numCopied, '  Duplicates found: ', numDuplicates)
 
 if __name__ == '__main__':
-    sort_files_by_month(seperating_char='\\', valid_file_types=('.jpg','.jpeg','.png','.avi','.mov'))
+    sort_files_by_month(copyFromDir='/imports/', copyToDir='/photo-archive/', valid_file_types=('.jpg','.jpeg','.png','.avi','.mov','.thm','.tiff'))
